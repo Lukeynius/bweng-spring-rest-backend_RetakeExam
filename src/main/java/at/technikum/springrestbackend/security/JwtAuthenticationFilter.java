f8f1400328a53e642257d9739ab5d23d1df78c0e
Lukeynius <lukas.prager11@gmail.com>
2026-03-26 21:44:56 +0100

    added dir security & files and updated application.properties & pom.xml
    added: directory security & Jwt and Config files
    updated: application.properties

---
+++ src/main/java/at/technikum/springrestbackend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+//#######################################################################
+//#######################################################################
+//#######################################################################
+// imports & packages
+package at.technikum.springrestbackend.security;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+//#######################################################################
+//#######################################################################
+//#######################################################################
+// class
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    )throws ServletException, IOException {
+        String token = extractTokenFromRequest(request);
+        if(token != null && jwtTokenProvider.validateToken(token)){
+            UUID userId = jwtTokenProvider.getUserIdFromToken(token);
+            String username = jwtTokenProvider.getUsernameFromToken(token);
+            String role = jwtTokenProvider.getRoleFromToken(token);
+            SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_"+role);
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userId, null, List.of(authority));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractTokenFromRequest(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+}
---
+++ src/main/java/at/technikum/springrestbackend/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+//#######################################################################
+//#######################################################################
+//#######################################################################
+// imports & packages
+package at.technikum.springrestbackend.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+
+//#######################################################################
+//#######################################################################
+//#######################################################################
+// class
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        // public (anonym)
+                        .requestMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/surveys").permitAll()
+                        // Swagger/openAPI
+                        .requestMatchers("/swagger.html", "swagger-ui/**", "/v3/api-docs/**", "/api/**").permitAll()
+                        // Admin only
+                        .requestMatchers(HttpMethod.GET, "/api/users").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/users/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/surveys").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/surveys/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/surveys/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/surveys/**").hasRole("ADMIN")
+                        // all other requests
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+}
